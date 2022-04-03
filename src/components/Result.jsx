da42f3bebb871fd66277d42a1752fc8907bbffbc
juhyoungryu <thinkoutside2004@gmail.com>
2022-04-03 14:06:19 +0900

    ui 그리는 중

--- src/components/Result.jsx
+++ src/components/Result.jsx
@@ -2,9 +2,31 @@ import React from "react";
 import "./Result.css";
 
 const Result = (props) => {
+  const mbti = () => {
+    console.log(props.mbti);
+  };
+
+  mbti();
+
   return (
     <div className="Result">
-      <div></div>
+      <section className="rsTop">
+        {/* 로고 넣을 위치 */}
+        <p className="title">스물다섯 스물하나</p>
+        <p className="sub">인물 유형 테스트</p>
+      </section>
+      <section className="rsMid">
+        {/* 일러스트 */}
+        <div className="imgCon" />
+        {/* 유형 이름 */}
+        <p>유형 이름</p>
+        {/* 유형 특징 */}
+        <p>유형 특징</p>
+      </section>
+      <section className="rsBot">
+        {/* 재검사 버튼 */}
+        {/* sns btn */}
+      </section>
     </div>
   );
 };
@@ -15,5 +37,5 @@ export default Result;
 // 유형 이름 - 극중 역할 이름
 // 특징 나열
 // 잘 맞는 유형 / 안 맞는 유형
-// 1번 -> 재검사 버튼
-// 2번 -> 개발자 인스타 & 깃허브
+// 재검사 버튼
+// 개발자 인스타 & 깃허브
