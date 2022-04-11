a6694a9d104a6e976b09fb64a6154bc15d5fcb5a
juhyoungryu <thinkoutside2004@gmail.com>
2022-04-11 11:00:00 +0000

    결과창 작업 중

--- src/data.js
+++ src/data.js
@@ -1,12 +1,23 @@
-export const yelloTulip = {
-  name: "외유내강 노랑튤립",
-  img: "",
-  explain: [
-    "대충 이런 설명1",
-    "대충 이런 설명2",
-    "대충 이런 설명3",
-    "대충 이런 설명4",
-  ],
-  good: { name: "잘 맞는 유형", img: "" },
-  bad: { name: "안 맞는 유형", img: "" },
-};
+function TypeMake(name, img, explain) {
+  this.name = name;
+  this.img = img;
+  this.explain = [...explain];
+}
+
+export const yelloTulip = new TypeMake("외유내강 노랑튤립", "", [
+  "대충 이런 설명1",
+  "대충 이런 설명2",
+  "대충 이런 설명3",
+  "대충 이런 설명4",
+]);
+
+// export const yelloTulip = {
+//   name: "외유내강 노랑튤립",
+//   img: "",
+//   explain: [
+//     "대충 이런 설명1",
+//     "대충 이런 설명2",
+//     "대충 이런 설명3",
+//     "대충 이런 설명4",
+//   ],
+// };
